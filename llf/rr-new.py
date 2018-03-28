ea017f3f671176575f9b403edf56943d9bafa780
Ashish Kumar <35109474+bashish2711@users.noreply.github.com>
2018-03-28 01:31:02 +0000

    Update rr-new.py

--- llf/rr-new.py
+++ llf/rr-new.py
@@ -4,43 +4,61 @@
 import cgi, cgitb
 print "Content-type:text/html\r\n\r\n"
 print """
-
-<form action="" method = "post" > 
-<input type = "radio" name = "sch" value = "RR" /> Round Robin
-<input type = "radio" name = "sch" value = "EDF" /> EDF 
-<input type = "submit" value = "Choose" />
-</form>
+<form action="" method = "post" >
+<input type = "radio" name = "sch" value = "RR" > Round Robin
+<input type = "radio" name = "sch" value = "EDF"> EDF 
+<input type = "radio" name = "sch" value = "LLF" > LLF
+<input type = "radio" name = "sch" value = "FCFS"> FCFS
+<input type="submit" name = "new" value="Add">
+<input type="submit" name = "new" value="Remove"> 
 """
 print """
-<form action="" method = "post" >
-  <input type="submit" name = "new" value="Add">
-</form>
-    
-"""
-form = cgi.FieldStorage() 
-#
+         <style>
+        table, th, td {
+        border: 1px solid black;
+        }
+        </style>
+        </head>
+        <body>
+
+        <table id="myTable" >
+        <caption>Monthly savings</caption>
+         <tr>
+         <th>Month</th>
+         <th>Savings</th>
+          </tr>
+          <tr>
+            <td>January</td>
+            <td>$100</td>
+          </tr>
+          <tr>
+         <td>February</td>
+          <td>$50</td>
+          </tr>
+        </table>
+        """
+
+form = cgi.FieldStorage()
+policy = form.getvalue('sch')
 if form.getvalue('new'): 
    sub = form.getvalue('new')
    print "<h1> %s </h1>" %sub
+   print "<h1> %s </h1>" %policy
    if sub == "Add":
-      print """
-      <form action="/Ferret/Ashish/action_page.php" method = "post" >
-      <input type="text" name="email" size="1"> 
-      <input type="text" name="pin" maxlength="4" size="1">
-      <input type="submit" value="Submit">
-      </form>
-      """ 
+      if policy == "RR":
+         print """
+         <action="" method = "post" >
+         <pre> Task-Name:Arrival-Time(A.T):Burst Time(B.T):Period (P):Deadline (D):Real (Hard or Soft): </pre>
+         <input type="text" name="task" value="Task" size="3"> 
+         <input type="text" name="start_time" maxlength="4" size="3">
+         <input type="text" name="burst_time" size="3"> 
+         <input type="text" name="period" size="3">
+         <select name = "dropdown">
+         <option value = "Hard" selected>Hard Real Time</option>
+         <option value = "Soft">Soft Real Time</option>
+         </select>
+	 </form>
+          """
 
-# Import modules for CGI handling 
-# Get data from fields
-if form.getvalue('sch'):
-   subject = form.getvalue('sch')
-   print "<h1> %s </h1>" %subject
-   if subject == "RR":
-       print """
-    <form action="/cgi-bin/Ferret/Ashish/rr.py">
-        <input type="submit" value="Open Script">
-    </form>
-        """
-else:
-   subject = "Not set"
+	
+
